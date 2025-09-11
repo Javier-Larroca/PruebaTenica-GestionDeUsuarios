d52768f9a2072300f6b9cd4b7d21d63089ef7167
Javier Larroca <javier-larroca@hotmail.com>
2025-09-11 16:23:15 -0300

    Creación del proyecto

---
+++ PruebaTecnicaAtica/GestionUsuarios/Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GestionUsuarios.Entities
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool ConfirmedEmail { get; set; }
+        public bool Active { get; set; }
+        public DateTime Creation {  get; set; }
+        public DateTime LastAccess { get; set; }
+    }
+}
